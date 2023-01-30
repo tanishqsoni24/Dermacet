5bcda9a6da39273367fc5c1440b9a347ed217098
Tanishq Soni <tanishqsoni0309@gmail.com>
2023-01-30 11:30:04 +0530

    ServiceLvlPush

--- dermacet/src/App.js
+++ dermacet/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar'
 import LandingPage from './components/LandingPage'
+import ServiceLvl from './components/ServiceLvl'
 
 function App() {
   return (
     <>
       <Navbar/>
       <LandingPage/>
+      <ServiceLvl/>
     </>
   );
 }
