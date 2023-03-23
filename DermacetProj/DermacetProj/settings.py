622982b181c49420ee0a553db26c771e1be75b1c
Tanishq Soni <tanishqsoni0309@gmail.com>
2023-03-23 22:40:50 +0530

    Dev: Email OAuth

--- DermacetProj/DermacetProj/settings.py
+++ DermacetProj/DermacetProj/settings.py
@@ -135,3 +135,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
